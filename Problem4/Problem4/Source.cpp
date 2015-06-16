a3bc5446022112f08a9ebeff726dd9873fd7e8e1
wWDosWw <dre.lacour716@gmail.com>
2015-06-16 17:56:10 -0400

    Problem 4 Done & commented

--- Problem4/Problem4/Source.cpp
+++ Problem4/Problem4/Source.cpp
@@ -5,7 +5,7 @@ Problem 3*/
 using namespace std;
 #include <iostream>
 #include <stdio.h>
-#include <stdlib.h> //has itoa in it
+#include <stdlib.h> //has _itoa_S in it which converts int to string
 
 int main(){
 	int firstNumber = 999;
@@ -14,15 +14,15 @@ int main(){
 	int total;
 	char value[10];
 	bool flag = true;
-	while (firstNumber>0){
-		total = firstNumber*secondNumber;
-		_itoa_s(total, value, 10);
+	while (firstNumber>0){											//Multiplies out every number
+		total = firstNumber*secondNumber;							//puts it in a total
+		_itoa_s(total, value, 10);									//then checks whether or not it is a palidrome
 		for (int i = 0; i < strlen(value)/2; i++){
 			if (value[i] != value[strlen(value) - 1 - i]){
 				break;
 			}
-			if (i==strlen(value)/2-1 && total > largestNum){
-				largestNum = total;
+			if (i==strlen(value)/2-1 && total > largestNum){		//if it is a palidrome and is greater than the previous palidrome
+				largestNum = total;									//becomes the new largest total
 			}
 		}
 
