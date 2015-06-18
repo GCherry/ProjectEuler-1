bc5821dfe4b99d1feba108e8e7f6320ff42782de
wWDosWw <dre.lacour716@gmail.com>
2015-06-17 23:37:35 -0400

    Problem 6 done & commented

--- Problem6/Problem6/Source.cpp
+++ Problem6/Problem6/Source.cpp
@@ -1,6 +1,7 @@
 /*James Andre LaCour
 ProjectEuler.net
 Problem 6*/
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,14 +12,14 @@ int main(){
 	int sumOfSquares = 0;
 	int squaresOfSum = 0;
 	int difference;
-	for (int i = 1; i <= 100; i++){
-		sumOfSquares = sumOfSquares + pow(i, 2);
+	for (int i = 1; i <= 100; i++){			//100 is basically a variable for whatever you want to do
+		sumOfSquares = sumOfSquares + pow(i, 2);	// 1^2 + 2^2 + 3^2 +......+n^2
 	}
 	for (int j = 1; j <= 100; j++){
-		squaresOfSum =  squaresOfSum + j;
+		squaresOfSum =  squaresOfSum + j;		//1+2+3+......+n
 	}
-	squaresOfSum = pow(squaresOfSum, 2);
-	difference = squaresOfSum-sumOfSquares;
+	squaresOfSum = pow(squaresOfSum, 2);		//then squares ^^that number
+	difference = squaresOfSum-sumOfSquares;		//difference for them joints
 	cout << difference << '\n';
 	system("pause");
 	return 0;
