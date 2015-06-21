ba001a169462b2988cc78beb6e4543b660a57b21
wWDosWw <dre.lacour716@gmail.com>
2015-06-21 14:22:39 -0400

    Improved Runtime

--- Problem3/Problem3/Source.cpp
+++ Problem3/Problem3/Source.cpp
@@ -4,30 +4,22 @@ Problem 3*/
 
 #include <iostream>
 #include <stdio.h>
+#include "../../Functions.h"
 using namespace std;
 
 int main() {
-	bool flag = true;		//Flag to stay in the loop, once we have prime number, turns to false
-	int finalNumber = 600851475143;
-	int isPrime = 0;		//if isPrime==2 then we got our answer, any over and its not prime
-	while (flag == true){
-		finalNumber--;
-		if (600851475143 % finalNumber == 0){			//if mod ==0 then it is a factor
-			isPrime = 0;
-			for (int i = finalNumber; i > 0; i--){		//check to see if factor is a prime number
-				if (finalNumber%i == 0){
-					isPrime++;
-				}
-				if (isPrime ==3){
-					break;
-				}
-			}
-			if (isPrime == 2){
-				flag = false;
+	long long finalNumber = 600851475143;
+	long long answer;
+	for (long long i = 2; i < finalNumber / 2; i++){
+		if (finalNumber % i == 0){
+			if (isPrime(finalNumber/i)){
+				answer = finalNumber / i;
+				break;
 			}
 		}
 	}
-	cout << finalNumber << "\n";
+
+	cout << answer << "\n";
 	system("pause");
 	return 0;
 }
